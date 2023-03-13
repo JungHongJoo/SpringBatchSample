b8f7f1b6dfe31c6412759e12460870320c976822
정홍주 <jhongjoo@hanssem.com>
2023-03-13 16:56:46 +0900

    GIT HUB COMMIT TEST

--- SpringBatchSample/batch.sample/src/main/java/com/boot/batch/sample/Application.java
+++ SpringBatchSample/batch.sample/src/main/java/com/boot/batch/sample/Application.java
@@ -1,8 +1,12 @@
 package com.boot.batch.sample;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableBatchProcessing
 @SpringBootApplication
 public class Application {
 
